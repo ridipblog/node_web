620f0d89529d90099f7b3e48d281e820c9d06650
ridipblog <memorytemp5@gmail.com>
2021-04-30 10:52:16 +0530

    heroku 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT || 3000;
 app.get('/',(req,res,next)=>{
-        res.send('welcome to firebase');
+        res.send('welcome to heroku i am very happy');
 });
 app.listen(port,()=>{
         console.log('Server');
